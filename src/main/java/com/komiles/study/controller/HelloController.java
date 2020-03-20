b994bb3598fb33085b381e016f311ab5969f8c39
kongming <komiles@163.com>
2020-03-20 17:07:39 +0800

    *添加时间戳

--- src/main/java/com/komiles/study/controller/HelloController.java
+++ src/main/java/com/komiles/study/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.komiles.study.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,17 @@ public class HelloController {
     public void config()
     {
         log.info("【配置中心】- 测试");
+    }
+
+    @GetMapping("/time")
+    public void time()
+    {
+        Date date = new Date();
+        Long res = System.currentTimeMillis()/1000;
+        log.info("时间为：{}", res);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        log.info("date:{}",simpleDateFormat.format(date));
     }
 
 }
